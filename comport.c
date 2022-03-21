7db31d1ae4930832d3bae83067f0a66717e39532
IOhannes m zmölnig <zmoelnig@iem.at>
2022-03-21 09:40:46 +0100

    whitespace

--- comport.c
+++ comport.c
@@ -61,9 +61,11 @@ typedef struct comport
     struct termios  oldcom_termio; /* save the old com config */
     struct termios  com_termio; /* for the new com config */
 #endif
+
     t_symbol        *serial_device;
     char            serial_device_prefix[FILENAME_MAX];/* the device name without the number */
     short           comport; /* holds the comport # */
+
     t_float         baud; /* holds the current baud rate */
     t_float         data_bits; /* holds the current number of data bits */
     t_float         parity_bit; /* holds the current parity */
@@ -71,6 +73,7 @@ typedef struct comport
     int             xonxoff; /* nonzero if xonxoff handshaking is on */
     int             ctsrts; /* nonzero if ctsrts handshaking is on */
     int             hupcl; /* nonzero if hang-up on close is on */
+
     short           rxerrors; /* holds the rx line errors */
     t_clock         *x_clock;
     int             x_hit;
@@ -1153,7 +1156,7 @@ static void comport_tick(t_comport *x)
                  * port gets interrupted, like if the USB gets yanked
                  * out or a bluetooth connection drops */
                 if(x->x_retry_count < x->x_retries)
-                { 
+                {
                     t_atom retrying_atom;
                     SETFLOAT(&retrying_atom, x->x_retry_count);
                     outlet_anything(x->x_status_outlet, gensym("retrying"), 1, &retrying_atom);
@@ -1172,7 +1175,7 @@ static void comport_tick(t_comport *x)
                     comport_close(x);
                 }
             }
-            else 
+            else
                 whicherr = errno;
         }
 #endif /* _WIN32 */
@@ -1241,13 +1244,13 @@ static int write_serial(t_comport *x, unsigned char  serial_byte)
     if(x->comhandle == INVALID_HANDLE_VALUE)
     {
         comport_verbose ("[comport]: Serial port is not open");
-        return 0;         
+        return 0;
     }
     else if(x->x_outbuf_wr_index < x->x_outbuf_len)
     {
         x->x_outbuf[x->x_outbuf_wr_index++] = serial_byte;
         return 1;
-    }    
+    }
     /* handle overrun error */
     pd_error (x, "[comport]: buffer is full");
     return 0;
@@ -1259,7 +1262,7 @@ static int write_serials(t_comport *x, unsigned char *serial_buf, int buf_length
     if(x->comhandle == INVALID_HANDLE_VALUE)
     {
         pd_error (x, "[comport]: Serial port is not open");
-        return 0;         
+        return 0;
     }
     for (i = 0; ((i < buf_length) && (x->x_outbuf_wr_index < x->x_outbuf_len)); ++x->x_outbuf_wr_index, ++i)
         x->x_outbuf[x->x_outbuf_wr_index] = serial_buf[i];
@@ -2054,4 +2057,3 @@ void comport_setup(void)
         "LGPL 1998-2015,  Winfried Ritsch and others (see LICENSE.txt)\n"
         "Institute for Electronic Music - Graz");
 }
-
